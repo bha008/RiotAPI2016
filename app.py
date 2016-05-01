2604ea1942d8b2d9d7b5ec2d342a4f0008d585bd
brendanha <datbdude@gmail.com>
2016-05-01 14:19:39 -0700

    created form template for initial UI; redirected form to proper endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, Response
+from flask import Flask, render_template, redirect, url_for, Response, request
 from riotAPIwrapper import ritoWrap
 import os
 # ...
@@ -7,16 +7,19 @@ app = Flask(__name__)
 rw = ritoWrap(os.environ['RIOT_API_KEY'])
 # ...
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index.html',
-                           score=rw.request_mastery_score('19659789'),
-                           top_champs=rw.request_top_champs('19659789'))
-    # return render_template('index.html')
+    error = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        region = request.form['region']
+        return redirect(url_for('info', username=username, region=region))
+
+    return render_template('form.html')
 
 @app.route('/info/<region>/<username>')
 def info(region=None,username=None):
-    print(region,username)
+    print(region, username)
     summ_id = str(rw.request_id_from_name(username,region=region))
     return render_template('index.html',
                            score=rw.request_mastery_score(summ_id, region=region),
