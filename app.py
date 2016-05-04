153c56ef83baa1bcfa7c794828d33a30eeeae2d1
bho008 <bho008@ucr.edu>
2016-05-04 03:06:39 -0700

    parsed json, added names and images to second page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, Response, request
+from flask import Flask, render_template, redirect, url_for, Response, request, json
 from riotAPIwrapper import ritoWrap
 import os
 # ...
@@ -36,11 +36,44 @@ def info(region=None, username=None):
     :return:
     """
     summ_id = str(rw.request_id_from_name(username, region=region))
-    top_Champs=rw.request_top_champs(summ_id, region=region)
+    top_Champs, champ_id0, champ_id1, champ_id2=rw.request_top_champs(summ_id, region=region)
+    with open('static/assets/riot_champion_data/lol-champions_6_8_1.json') as riot_champ_file:
+        json_data = json.load(riot_champ_file)
+        print 'keys in temp:', json_data.keys()
+        print(json_data['data']['35'].get('image'))
+        print(json_data['data'][champ_id0]['image'].get('full'))
+        print(json_data['keys'].get(champ_id0))
+        print(json_data['keys'].get(champ_id1))
+        print(json_data['keys'].get(champ_id2))
+
+        champ_name_0 = json_data['keys'].get(champ_id0)
+        #champ_name_png_0 = 'http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/' + \
+        #                   json_data['data'][champ_id0]['image'].get('full') + \
+        #                    '.png'
+        champ_name_png_0 = json_data['data'][champ_id0]['image'].get('full')
+
+        champ_name_1 = json_data['keys'].get(champ_id1)
+        champ_name_png_1 = json_data['data'][champ_id1]['image'].get('full')
+
+        champ_name_2 = json_data['keys'].get(champ_id2)
+        champ_name_png_2 = json_data['data'][champ_id2]['image'].get('full')
+
+    riot_champ_file.close()
+
     return render_template('index.html',
                            username=username,
                            score=rw.request_mastery_score(summ_id, region=region), 
-						   top_champs=top_Champs)
+						   top_champs=top_Champs,
+                           champ_ID0=champ_id0,
+                           champ_ID1=champ_id1,
+                           champ_ID2=champ_id2,
+                           champ_name_0 = champ_name_0,
+                           champ_name_png_0 = champ_name_png_0,
+                           champ_name_1=champ_name_1,
+                           champ_name_png_1=champ_name_png_1,
+                           champ_name_2 = champ_name_2,
+                           champ_name_png_2 = champ_name_png_2
+                           )
 
 if __name__ == '__main__':
     app.run(debug=True)
